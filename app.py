eff88f8a5c5bb989120f56047022aacfccfff6fc
AlbertUlysses <albertulysseschavez@gmail.com>
2020-04-07 19:27:52 -0700

    feat: update models

--- app.py
+++ app.py
@@ -32,12 +32,20 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.String(120))
+    address = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String())
+    image_link = db.Column(db.String(500))
+    past_shows = None
+    upcoming_shows = None
+    past_shows_count = None
+    upcoming_shows_count = None
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
